7dce0870603be5c60467302c2858f87cccf857a1
wajihulhasan01 <wajihulhasan01@gmail.com>
2025-01-02 08:18:27 +0500

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 function generateTable() {
+    const range = document.getElementById('range').value;
     const num = document.getElementById('num').value;
     const tableContainer = document.getElementById('table-container');
     tableContainer.innerHTML = '';
@@ -8,12 +9,12 @@ function generateTable() {
         tableContainer.innerHTML = 'Invalid input! Please enter a number between 1 and 100.';
         return;
     }
-
+    
     const table = document.createElement('table');
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= range; i++) {
         const row = document.createElement('tr');
         const cell1 = document.createElement('td');
-        cell1.textContent = `${num} x ${i}`;
+        cell1.textContent = `${num}  x  ${i}  =`;
         const cell2 = document.createElement('td');
         cell2.textContent = num * i;
         row.appendChild(cell1);
